6dbc4cdf4f67327aab83d088020d124c44fb8ace
Ryan Harley <rharley.dev@gmail.com>
2020-11-20 10:33:12 -0600

    starting C++

---
+++ sources/main.cpp
@@ -0,0 +1,14 @@
+// begin config options for C++
+#include <iostream>
+
+using namespace std;
+// end config options
+
+// main func to contain code
+int main()
+{
+  printf("Hello World");
+  cout << "Hello Again" << endl;
+  cout << "Hello And Again" << endl;
+  return 0; // needed to run code
+}
