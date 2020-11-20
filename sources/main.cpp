bfd32019772828de4fcd54875711738abcbbf211
Ryan Harley <rharley.dev@gmail.com>
2020-11-20 12:57:15 -0600

    setting variables

--- sources/main.cpp
+++ sources/main.cpp
@@ -7,8 +7,18 @@ using namespace std;
 // main func to contain code
 int main()
 {
+  // setting variables
+  char grade = 'A';     // character var uses single quotes
+  string name = "John"; // specify the type then var name
+  int age;              // init intiger type with the name CANNOT USE DESIMALS
+  age = 50;
+  double gpa = 3.5; // stores more dec than floats for specificity
+  bool isMale = true;
+
+  cout << "Hello " << name << endl; 
+  cout << "Hello " << age << " Again" << endl;
+
   printf("Hello World");
-  cout << "Hello Again" << endl;
-  cout << "Hello And Again" << endl;
+
   return 0; // needed to run code
 }
