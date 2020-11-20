44d573b09180ca54c806824149e01b52c97a6550
Ryan Harley <rharley.dev@gmail.com>
2020-11-20 13:29:53 -0600

    finishing numbers

--- sources/number.cpp
+++ sources/number.cpp
@@ -1,5 +1,6 @@
 // begin config options for C++
 #include <iostream>
+#include <cmath> // importing math functions
 
 using namespace std;
 // end config options
@@ -11,6 +12,9 @@ int main()
   num++;     // adding 1 to the var
   num += 50; // adding 50 to num
 
+  cout << pow(2, 5) << endl;  // raising 2 to the power of 5
+  cout << sqrt(36) << endl;   // squaring
+  cout << fmax(3, 4) << endl; // comparing to find largest num
   cout << num << endl;
   cout << 40 % 7; // modulous operator that gives the remainder
 
